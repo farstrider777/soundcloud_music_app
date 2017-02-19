8ed2f4a8f67503e3c6ea30f821193175dcc2ebb3
Joshua Bradford <jjyet@yahoo.com>
2017-02-19 08:05:41 -0500

    added placekittens to be the images if artist image is null :)

--- src/js/templates.js
+++ src/js/templates.js
@@ -1,13 +1,23 @@
+/* eslint-disable */
+
 import $ from 'jquery';
 
 function buildSearchResultBox(artistPictureUrl, songTitle, artistName, count){
+
+  if(artistPictureUrl === null){
+    artistPictureUrl = "http://placekitten.com/130/130"
+  }
+
+
   $(".search-container").append(`
     <div class="search-results-container">
-      <img id="select-${count}" src="${artistPictureUrl}"></img>
+      <img id="select-${count}"src="${artistPictureUrl}"></img>
       <div class="song-title">${songTitle}</div>
       <div class="artist-name">${artistName}</div>
     </div>
     `);
   }
+
+  //${if (artistPictureUrl = null){console.log("yes")}}
 
   export { buildSearchResultBox };
